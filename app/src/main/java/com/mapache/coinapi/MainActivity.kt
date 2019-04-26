55ae577dd3c13d8818c08d7218f233530b976669
MaPache07 <00170517@uca.edu.sv>
2019-04-25 22:51:59 -0600

    Se ha implementado la API y se han agregado todas las dependencias

--- app/src/main/java/com/mapache/coinapi/MainActivity.kt
+++ app/src/main/java/com/mapache/coinapi/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.mapache.coinapi
 
+import android.content.ContentValues
+import android.database.DatabaseUtils
+import android.os.AsyncTask
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
@@ -9,11 +12,22 @@ import android.view.MenuItem
 import android.support.v4.widget.DrawerLayout
 import android.support.design.widget.NavigationView
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.Toolbar
 import android.view.Menu
+import com.google.gson.Gson
+import com.mapache.coinapi.data.Database
+import com.mapache.coinapi.data.DatabaseContract
+import com.mapache.coinapi.models.Coin
+import com.mapache.coinapi.models.CoinList
+import com.mapache.coinapi.utilities.AppConstants
+import com.mapache.coinapi.utilities.NetworkUtil
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
+    var dbHelper = Database(this)
+    lateinit var coinList : CoinList
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +49,55 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
 
         navView.setNavigationItemSelectedListener(this)
+        FetchCoinTask().execute(AppConstants.COIN_LINK)
+    }
+
+    fun initRecycler(){
+        var viewManager = LinearLayoutManager(this)
+        if(this.resources.configuration.orientation == 2 || this.resources.configuration.orientation == 4){
+            viewManager = LinearLayoutManager(this)
+        }
+        else{
+            viewManager = GridLayoutManager(this, 2)
+        }
+        /*recyclerview.apply {
+            adapter = CoinAdapter(coinList.coins, {coin: Coin -> clickedCoin(coin)})
+            layoutManager = viewManager
+        }*/
+    }
+
+    inner class FetchCoinTask : AsyncTask<String, Void, String>(){
+        override fun doInBackground(vararg params: String?): String? {
+            if(params.size == 0){
+                return null
+            }
+            val db = dbHelper.writableDatabase
+            var coinApi = NetworkUtil().buildUrl(params[0]!!)
+            var listCoin = NetworkUtil().getResponseFromHttpUrl(coinApi)
+            coinList = Gson().fromJson(listCoin, CoinList::class.java)
+            if(DatabaseUtils.queryNumEntries(db, "coin").toInt() != coinList.coins.size){
+                db.delete("coin", null, null)
+                for(coin: Coin in coinList.coins){
+                    val values = ContentValues().apply {
+                        put(DatabaseContract.CoinEntry.COLUMN_NAME, coin.name)
+                        put(DatabaseContract.CoinEntry.COLUMN_COUNTRY, coin.country)
+                        put(DatabaseContract.CoinEntry.COLUMN_VALUE, coin.value)
+                        put(DatabaseContract.CoinEntry.COLUMN_VALUE_US, coin.values_us)
+                        put(DatabaseContract.CoinEntry.COLUMN_YEAR, coin.year)
+                        put(DatabaseContract.CoinEntry.COLUMN_VALUE, coin.value)
+                        put(DatabaseContract.CoinEntry.COLUMN_ISAVAILABLE, coin.isAvailable)
+                        put(DatabaseContract.CoinEntry.COLUMN_IMG, coin.img)
+                    }
+                }
+            }
+            return null
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            initRecycler()
+        }
+
     }
 
     override fun onBackPressed() {
@@ -87,5 +150,10 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         drawerLayout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    override fun onDestroy() {
+        dbHelper.close()
+        super.onDestroy()
     }
 }
