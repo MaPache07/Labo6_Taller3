d829c00a16faa8b816e370d1c82089c8c3e3a7b1
MaPache07 <00170517@uca.edu.sv>
2019-04-25 23:13:27 -0600

    Se ha agregado el CoinAdapter

---
+++ app/src/main/java/com/mapache/coinapi/CoinAdapter.kt
@@ -0,0 +1,35 @@
+package com.mapache.coinapi
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.mapache.coinapi.models.Coin
+import kotlinx.android.synthetic.main.list_item_coin.view.*
+
+class CoinAdapter(val coins:List<Coin>, val clickedCoinListener: (Coin) -> Unit) : RecyclerView.Adapter<CoinAdapter.ViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, p1: Int): CoinAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_coin, null, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = coins.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(coins[position])
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        fun bind(item: Coin) {
+            itemView.text_coin.text = item.name
+            with(itemView){
+                Glide.with(itemView).load(item.img).placeholder(R.drawable.ic_launcher_background).into(itemView.img_coin)
+            }
+            itemView.setOnClickListener{
+                clickedCoinListener(item)
+            }
+        }
+    }
+}
