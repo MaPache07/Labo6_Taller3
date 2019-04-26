919a54cf6535acabdba7cc8d664ab61b1c10f0c2
Fernando <00017917@uca.edu.sv>
2019-04-25 22:47:16 -0600

    se agregaron paquetes models y data

---
+++ app/src/main/java/com/mapache/coinapi/models/Coin.kt
@@ -0,0 +1,10 @@
+package com.mapache.coinapi.models
+
+data class Coin (var _id:String,
+                 var name: String,
+                 var country: String,
+                 var value: Int,
+                 var values_us: Double,
+                 var year: Int,
+                 var isAvailable: Boolean,
+                 var img: String)
---
+++ app/src/main/java/com/mapache/coinapi/models/CoinList.kt
@@ -0,0 +1,6 @@
+package com.mapache.coinapi.models
+
+data class CoinList (
+    var ok : String,
+    var coins : ArrayList<Coin>
+)
