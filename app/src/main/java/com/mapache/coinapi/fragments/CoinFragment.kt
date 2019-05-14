8d3000543eb018e1b39becbb6feb20eff002fdb1
MaPache07 <00170517@uca.edu.sv>
2019-05-13 19:00:10 -0600

    Ya se muestra la bandera del pais respectivo a cada moneda

--- app/src/main/java/com/mapache/coinapi/fragments/CoinFragment.kt
+++ app/src/main/java/com/mapache/coinapi/fragments/CoinFragment.kt
@@ -1,14 +1,18 @@
 package com.mapache.coinapi.fragments
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.mapache.coinapi.R
 import com.mapache.coinapi.models.Coin
 import com.mapache.coinapi.utilities.AppConstants
 import kotlinx.android.synthetic.main.content_fragment.view.*
+import kotlinx.android.synthetic.main.nav_header_main.view.*
 
 class CoinFragment : Fragment(){
 
@@ -37,6 +41,20 @@ class CoinFragment : Fragment(){
         view.text_value_us_coin.text = coin.values_us.toString()
         view.text_year_coin.text = coin.year.toString()
         view.text_available_coin.text = coin.isAvailable.toString()
+        Glide.with(view).load(coin.img).into(view.coin_image)
+        Log.d("Hola", coin.country)
+        when(coin.country){
+            "Australia" -> view.nav_bar.setBackgroundResource(R.drawable.australia)
+            "Suiza" -> view.nav_bar.setBackgroundResource(R.drawable.suiza)
+            "Mexico" -> view.nav_bar.setBackgroundResource(R.drawable.mexico)
+            "Europa" -> view.nav_bar.setBackgroundResource(R.drawable.europa)
+            "Canada" -> view.nav_bar.setBackgroundResource(R.drawable.canada)
+            "Japon" -> view.nav_bar.setBackgroundResource(R.drawable.japon)
+            "Venezuela" -> view.nav_bar.setBackgroundResource(R.drawable.venezuela)
+            "El salvador" ->  view.nav_bar.setBackgroundResource(R.drawable.el_salvador)
+            "Inglaterra" -> view.nav_bar.setBackgroundResource(R.drawable.inglaterra)
+            "Corea del sur" -> view.nav_bar.setBackgroundResource(R.drawable.corea_sur)
+        }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
--- app/src/main/java/com/mapache/coinapi/utilities/AppConstants.kt
+++ app/src/main/java/com/mapache/coinapi/utilities/AppConstants.kt
@@ -3,4 +3,5 @@ package com.mapache.coinapi.utilities
 object AppConstants {
     val COIN_KEY = "CoinKey"
     val COIN_LINK = "https://apicoins.herokuapp.com/coin"
+    val IMG_AUSTRALIA = "https://www.comprarbanderas.es/images/banderas/400/23-australia_400px.jpg"
 }
